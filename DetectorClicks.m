8af8d85a3a1446bb5011828ec0516a8fc57e63cc
Alexander Marino <marinoalexander5@gmail.com>
2020-07-30 23:15:59 -0300

    Moved detection function to detector_umbral_2.m. Updated rms calculation for each click. Updated whole file plotting for debugging.

--- DetectorClicks.m
+++ DetectorClicks.m
@@ -20,18 +20,19 @@ clip = (2^(nbits))/2-1; % Determinar nivel de saturación
 th = 5;  % Determinar umbral SNR (linear ratio, not dB)
 dt_max = 5/1000; % Ventana de 5 ms entre cada detección
 NFFT = 512; % puntos de FFT
-han_window = hanning(150); % ventana hanning fft
+hann_window = hanning(512);
+tukey_window = tukeywin(512); % ventana tukey fft
 SVMmat = zeros(0,6); % SVM matrix input preallocation
 j = 0; % indice de detecciones totales
 %
 % Definir funcion de transferencia
+hydro_list = {'Reson','Antarctic_array'};
 tfnum = input('Select hydrophone used for recordings\n 1 = Reson dip hydrophone            2 = Antarctic Array\nNumber: ');
 while ~any([1, 2] == tfnum);
    fprintf('Invalid number, try again\n\n') 
    tfnum = input('Select hydrophone used for recordings\n 1 = Reson dip hydrophone            2 = Antarctic Array\nNumber: ');
 end
 %%
-counter = 0;
 for index = 1 : length(files);
     fprintf('File %d/%d\n Importando %s\n', index, length(files), files(index).name);
     to = 1; % Tiempo inicial del segmento a analizar (en muestras)
@@ -52,33 +53,26 @@ for index = 1 : length(files);
 %         fprintf('DEBUGG: SNR from singal: %d/ from envelope %d\n', rms(xx), nn_a);
         snr_a = abs(yy_a)/nn_a; % Relación señal / ruido
         %
-%%      %%%%%%%%%%%%%%%%% DEBUGGING START
+        % Uncomment función seleccionada (conservando ambas para comparación de desempeño)
+%         [jth,i1,i2] = detector_umbral(snr_a,tt,th,dt_max,yy_a,nn_a,to); % i1, i2 in samples
+        [jth,i1,i2,pks] = detector_umbral_2(yy_a, nn_a, th, dt_max, Fs, tt); % jth, i1, i2 in samples
+%%         % DEBUGGING PLOTS
+        % Whole segment
         figure (1)
-        plot(tt, yy_a, [tt(1) tt(end)], [th th], '--r')
-        [pks, locs] = findpeaks(abs(yy_a), Fs, 'MinPeakDistance', dt_max, 'MinPeakHeight', th*nn_a); % locs in time(tt) scale
-        % plot test
-        if any(pks);
+        plot(tt, yy_a, [tt(1) tt(end)], [th*nn_a th*nn_a], '--r')
+        if any(jth);
             hold on
-            plot(counter+locs , pks, 'Or');
-            plot ([0 10],[th th], '--r')
+            plot(to/Fs+jth/Fs , pks, 'Or');
         end
-        % crear branck para trabaajar en una version alternativa
-        % arreglar esta parte para ver como ajustar correctamente ventana tukey centrada en pks y locs(unificar locs(tiempo) con nro muestras (+256 y -256))
-        for i = (1:length(locs));
-            plot((counter+locs(i)-256/Fs:1/Fs:counter+locs(i)+255/Fs),tukeywin(512)*(pks(i)+1), '--k') % Centred 512 samples Tuckey window in click peak 
+        for i = (1:length(jth));
+            plot((to/Fs+(jth(i)-256)/Fs:1/Fs:to/Fs+(jth(i)+255)/Fs),tukey_window*(pks(i)+1), '--k') % Centred 512 samples Tuckey window in click peak 
             hold on
         end
-        counter = counter + 1;
-        to = to+Fs;
-    end
-end
-    
-%%        %%%%%%%%%%%% DEBUGING END
-        [jth,i1,i2] = detector_umbral(snr_a,tt,th,dt_max,yy_a,nn_a,to);
-%         % Redefinir segmento temporal si click queda en el límite de la división o no hay detecciones
-%         if isempty(jth);
-%             to = to+Fs;
-%             continue
+        %%
+        if isempty(jth);
+            to = to+Fs;
+            continue
+        % Redefinir segmento temporal si click queda en el límite de la división o no hay detecciones
 %         elseif ~any(abs(yy_a(1:jth(1)))<nn_a) && to>1;
 %             to = to-300;
 %             continue           
@@ -87,64 +81,70 @@ end
 %             if to > Ts.TotalSamples-Fs;
 %                 to = to-(length(xx)-i1(end));
 %             end
-%         end
+        end
 %         %%  Almacenamiento de información de cada click
 % %         Calibración función de transferencia
 %         if tfnum == 1;
 %             [FT,TXT,RAW]=xlsread('función de transferencia.xls',2); % Hidrófono Cethus
 %             FT = FT(:,6);
-%         elseif tnum == 2;
+%         elseif tfnum == 2;
 %             [FT,TXT,RAW]=xlsread('función de transferencia.xls',4);  % Arreglo hidrófonos SIO Antártida
 %             FT = FT(:,8); FT = FT(1:256);
 %         end
-%         for ii = k : k + length(jth) - 1;
-%             % Click Señal Temporal 
-%             click = xx(i1(ii-k+1):i2(ii-k+1));
-%             % Eliminar clicks saturados
-%             if isempty(find(click > clip, 1)) == 0;
-%                 Det(ii).signal = 'clipping';
-%                 continue
-%             end            
-%             Det(ii).signal = click;
-%             % Filename
-%             Det(ii).filename = files(index).name; 
-%             % Número de Click
-%             Det(ii).clickn = ii;
-%             % Tiempo Inicial de Click
-%             Det(ii).itime = datestr(tt(i1(ii-k+1))/(24*60*60), 'DD:HH:MM:SS.FFF');
-%             % Almacenar índices de número de archivo y número de detección
-%             indices(j+1,:) = [index ii];
-%             % Relación Señal a Ruido del click
-%             clickrms = sqrt(mean(abs(click).^2));
-%             Det(ii).snr = clickrms/nn_a;
-%             % Espectro de Click
-%             magnitude = 2*abs(fft(click.*hann_window,NFFT)./NFFT);
-%             magnitude = magnitude(1:NFFT/2);
-%             magnitudedB = (20*log10(magnitude)) + FT;
-%             Det(ii).spectrum = magnitudedB;
-%             % Parámetros Acústicos
-%             [pfreq, cfreq, dur10dB, bw3dB, bw10dB ] = acoustic_params(click, Fs, NFFT, magnitude, magnitudedB);
-%             Det(ii).pfreq = pfreq;
-%             Det(ii).cfreq = cfreq;
-%             Det(ii).dur10dB = dur10dB;
-%             Det(ii).bw3dB = bw3dB;
-%             Det(ii).bw10dB = bw10dB;
-%             %
-%             j = j+1;
-%         end
-%         k = k + length(jth);
-%         to = to+Fs;
-%     end
+        FT = 0;
+        for ii = k : k + length(jth) - 1;
+            % Click Señal Temporal 
+            click = xx(i1(ii-k+1):i2(ii-k+1)).*tukey_window;
+            % Eliminar clicks saturados
+            if isempty(find(click > clip, 1)) == 0;
+                Det(ii).signal = 'clipping';
+                continue
+            end            
+            Det(ii).signal = click;
+            % Filename
+            Det(ii).filename = files(index).name; 
+            % Número de Click
+            Det(ii).clickn = ii;
+            % Tiempo Inicial de Click
+            Det(ii).itime = datestr(tt(floor(i1(ii-k+1)))/(24*60*60), 'DD:HH:MM:SS.FFF');
+            % Almacenar índices de número de archivo y número de detección
+            indices(j+1,:) = [index ii];
+            % Relación Señal a Ruido del click
+            csum = cumsum(signal.^2); % Ventana del 95% de energia
+            [~,rmswin_start] = min(abs(win-0.025*csum(end)));
+            [~,rmswin_end] = min(abs(win-0.975*csum(end)));
+            clickrms = rms(click(rmswin_start:rmswin_end));
+            Det(ii).snr = clickrms/nn_a;
+            % Espectro de Click
+            magnitude = 2*abs(fft(click,NFFT)./NFFT);
+            magnitude = magnitude(1:NFFT/2);
+            magnitudedB = (20*log10(magnitude)) + FT;
+            Det(ii).spectrum = magnitudedB;
+            % Parámetros Acústicos
+            [pfreq, cfreq, dur10dB, bw3dB, bw10dB ] = acoustic_params(click, Fs, NFFT, magnitude, magnitudedB);
+            Det(ii).pfreq = pfreq;
+            Det(ii).cfreq = cfreq;
+            Det(ii).dur10dB = dur10dB;
+            Det(ii).bw3dB = bw3dB;
+            Det(ii).bw10dB = bw10dB;
+            %
+            % Hidrófono usado
+            Det(ii).hydrophone = hydro_list{tfnum};
+            j = j+1;
+        end
+        k = k + length(jth);
+        to = to+Fs;
+    end
 %       SVMmat = vertcat(SVMmat,[[Det.snr]' [Det.pfreq]' [Det.cfreq]' [Det.dur10dB]' [Det.bw3dB]' [Det.bw10dB]']); % Matriz de datos a clasificar
-%     File(index).Det = Det;
-%     %% Si hay detecciones agregar filename a .txt
-%     files_clicks = fopen('files-with-clicks.txt', 'a');
-%     f_content = fscanf(files_clicks, 's');
-%     if isempty(strfind(files(index).name, f_content)); % ver si file ya está incluido en la lista
-%         fprintf(files_clicks, '%s \n', files(index).name);
-%     end
-%     fclose(files_clicks);
-% end
+    File(index).Det = Det;
+    %% Si hay detecciones agregar filename a .txt
+    files_clicks = fopen('files-with-clicks.txt', 'a');
+    f_content = fscanf(files_clicks, 's');
+    if isempty(strfind(files(index).name, f_content)); % ver si file ya está incluido en la lista
+        fprintf(files_clicks, '%s \n', files(index).name);
+    end
+    fclose(files_clicks);
+end
 % SVMmat( ~any(SVMmat,2), : ) = [];
 % SVMmat_indexada = [indices SVMmat];
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- detector_umbral_2.m
+++ detector_umbral_2.m
@@ -1,6 +1,6 @@
-function detector_umbral_2(snr_a,th,dt_max, Fs)
+function [jth, i1, i2, pks] = detector_umbral_2(yy_a, nn_a, th, dt_max, Fs, tt)
 %%%
-%%%   [jth,i1,i2] = detector_umbral(snr_a,tt,th,dt_max,yy_a,nn_a,to)
+%%%   [jth,i1,i2] = detector_umbral_2(snr_a,tt,th,dt_max,yy_a,nn_a,to)
 %%%   
 %%%   Peak detection function for click extraction
 %%%   
@@ -14,18 +14,31 @@ function detector_umbral_2(snr_a,th,dt_max, Fs)
 %%%   to: initial time indicator
 %%%
 %%%   Retunrs:
-%%%   jth: 
+%%%   jth: vector of max peaks above threshold
 %%%   i1: vector of detection starting indices
 %%%   i2: vector of detection ending indices
 
-[pks, locs] = findpeaks(snr_a, Fs, 'MinPeakDistance', dt_max, 'MinPeakHeight', th, 'Threshold', 1);
-% plot test
-if any(pks);
-    figure
-    plot(snr_a)
-    hold on
-    plot(locs , pks);
-    plot ([0 Fs],[th th], '--r')
+[pks, locs] = findpeaks(abs(yy_a), Fs, 'MinPeakDistance', dt_max, 'MinPeakHeight', th*nn_a); % locs in time(tt) scale
+jth = locs*Fs; % turning to samples for easier indexing
+if any(jth)
+    i1 = jth - 256;
+    i2 = jth + 255;
+else
+    i1 = [];
+    i2 = [];
 end
 
+% % DEBUGGING PLOTS
+% % Whole segment
+% figure (1)
+% plot(tt, yy_a, [tt(1) tt(end)], [th*nn_a th*nn_a], '--r')
+% if any(locs);
+%     hold on
+%     plot(locs , pks, 'Or');
+% end
+% 
+% for i = (1:length(locs));
+%     plot((locs(i)-256/Fs:1/Fs:locs(i)+255/Fs),tukeywin(512)*(pks(i)+1), '--k') % Centred 512 samples Tuckey window in click peak 
+%     hold on
+% end
 
